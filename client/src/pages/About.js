5097319369066495935214de78f086471498e1c6
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-06-22 17:13:38 -0400

    Added content for the Mission Statement page and the About Us Page.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -20,14 +20,62 @@ class About extends Component {
 
             <Row>
                 <Col size = "md-12">
+                <div className="container-fluid styleAboutUsPage">
                 <h1 style={{textAlign: "center"}}>Who Are We?</h1>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <h2 style={{textAlign: "center"}}>Bruno Mazzitelli</h2>
+                <h5 style={{textAlign: "center"}}>Lead Visionary and Content Strategist</h5>
+                <h5 style={{textAlign: "center"}}>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                With years of leadership and management experience in the corporate sector, Bruno has played a crucial role in directing the path and planning stages for this project. <br/>
+                &nbsp;
+                &nbsp;
+                Kaibru's planning started late in 2018 and since then has taken many forms. This is the closest the team has come to having a solid, finished product.<br/>
+                &nbsp;
+                &nbsp;
+                With that in mind, there is still more work to be done and he is excited to continue charting Kaibru's course!
+                </h5>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <h2 style={{textAlign: "center"}}>Kai-Ann Fletcher</h2>
+                <h5 style={{textAlign: "center"}}>Principal Developer</h5>
+                <h5 style={{textAlign: "center"}}>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                With roughly two years of full-stack (MERN) development experience under her belt, Kai-Ann enjoys building various projects while learning new concepts. <br />
+                &nbsp;
+                &nbsp;
+                Building Kaibru from the ground up was not quite an easy process, but the team decided that it would make the vision and mission more meaningful. <br/>
+                &nbsp;
+                &nbsp;
+                With that in mind, Kai-Ann is excited to continue the building phase for Kaibru and is also excited to see the platform grow over time.
+                </h5>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <h2 style={{textAlign: "center"}}>Contact Us</h2>
+                <h5 style={{textAlign: "center"}}>
+                To report bugs or make recommendations for future features, please contact us at kb.ideas1@gmail.com
+                </h5>
+                </div>
                 </Col>
             </Row>
             
-            <Row>
+            {/* <Row>
                 <Col size = "md-12">
                 <h1>Bruno Mazzitelli</h1>
-                <p>Lead Business Strategist</p>
+                <p>Lead Visionary and Content Strategist</p>
                 </Col>
             </Row>
             
@@ -36,7 +84,7 @@ class About extends Component {
                 <h1>Kai-Ann Fletcher</h1>
                 <p>Principal Developer</p>
                 </Col>
-            </Row>
+            </Row> */}
             <Footer />
         </Container>
         </div>
--- client/src/pages/Mission.js
+++ client/src/pages/Mission.js
@@ -19,15 +19,44 @@ function About() {
 
             <Row>
                 <Col size = "md-12">
+                <div className="container-fluid styleMissionPage">
                 <h1 style={{textAlign: "center"}}>Making a Difference!</h1>
+                <h5 style={{textAlign: "center"}}>
+                Our aim is quite simple. We would like to create a safe space which allows for more mindful or meaningful consumerism with consideration for environmental and social improvement.<br/>
+                &nbsp;
+                &nbsp;
+                Even though our platform will evolve over time, this is our base goal. <br/>
+                &nbsp;
+                &nbsp;
+                In no way is this platform meant to force or manipulate anyone into feeling badly about their current spending habits. <br/>
+                &nbsp;
+                &nbsp;
+                As the creators of this platform, even our habits can be improved on a daily basis :). <br/>
+                &nbsp;
+                &nbsp;
+                As consumers, we collectively have the power to enact and enforce change, alongside holding corporations responsible for how they scale and produce. <br/>
+                &nbsp;
+                &nbsp;
+                We believe that with all things considered, large-scale production should never be done wholly at the expense of the environment or social well-being. <br/>
+                &nbsp;
+                &nbsp;
+                This is why our platform highlights responsible businesses which have these concepts woven into the fabric of their own company mission or vision. <br/>
+                &nbsp;
+                &nbsp;
+                By bringing them together in one hub, this makes it easier for consumers to select from a  wider variety, while raising awareness of the businesses out there trying to make a difference!
+                </h5>
+                </div>
                 </Col>
             </Row>
             
-            <Row>
+            {/* <Row>
                 <Col size = "md-12">
-                <p>More content here</p>
+                <div className="container-fluid styleHomePage">
+                <h5>More content here</h5>
+                </div>
                 </Col>
-            </Row>
+            </Row> */}
+            
             <Footer />
         </Container>
         </div>     
